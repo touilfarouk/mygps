27bbe9c5b681d2ac7be4bc68521936814ec1d256
Farouk Touil <farouktouil@hotmail.com>
2025-06-01 10:26:38 +0200

    some corrections

--- app/src/main/java/gaur/himanshu/gpstracker/service/LocationService.kt
+++ app/src/main/java/gaur/himanshu/gpstracker/service/LocationService.kt
@@ -40,7 +40,7 @@ data class Coordinates(
 object ApiClient {
     private val retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("https://onta.dz/api/location/") // Replace with your real URL
+            .baseUrl("")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
